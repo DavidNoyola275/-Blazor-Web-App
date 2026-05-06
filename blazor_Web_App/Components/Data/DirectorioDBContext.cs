7ba9ed7ccb1cfe26236f20eeed5eb4bca4dcf700
David Noyola <lnoyola@ucol.mx>
2026-05-06 14:55:51 -0600

    Actualizacion

---
+++ blazor_Web_App/Components/Data/DirectorioDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Directorio.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace practica1.Components.Data
+{
+    public class DirectorioDBContext(DbContextOptions<DirectorioDBContext>options): DbContext(options){
+        public DbSet<Persona>Personas { get; set; }
+    }
+}
--- blazor_Web_App/Program.cs
+++ blazor_Web_App/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using practica1.Components;
+using practica1.Components.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext < DirectorioDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
